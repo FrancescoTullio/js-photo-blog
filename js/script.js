3e00f72e164ecf4544b730fba949e69925b20c9a
FrancescoTullio <francescotullio1992@gmail.com>
2024-11-20 12:40:30 +0100

    feat:esercizio base

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 const greedElem = document.getElementById("griglia");
+const hoverElem = document.getElementById("hover-section");
+const closeBtnElem = document.getElementById("btn-close");
+const hoverImgElem = document.getElementById("hover-img");
+console.log(hoverImgElem)
+console.log(closeBtnElem);
 // console.log(greedElem);
 let innerGriglia = "";
 let arrayData = [];
@@ -10,20 +15,43 @@ axios.get(`https://jsonplaceholder.typicode.com/photos?_limit=6`, { timeout: 100
     // console.log(arrayData);
     arrayData.forEach((elem) => {
         const { title, url } = elem;
+        // console.log(globalUrl)
         // console.log(title);
         console.log(url);
-        innerGriglia += `<div class="col-12 col-md-6 col-lg-4">
-        <div class="ms-bg-white h-100 relativ-pos"> 
+        innerGriglia +=`<div class="col-12 col-md-6 col-lg-4">
+        <div class="ms-card h-100 relativ-pos" data-url="${url}"> 
         <div class="absolute-pos debug cirle rounded-circle"></div>
             <div class="container">
-                <img src="${url}" class="card-img-top pt-3" alt="descrizione" >
+                <img src="${url}" class="card-img-top pt-3" alt="descrizione">
             
             <div class="card-body pt-3 pb-5">
                 <h5 class="card-title">${title}</h5>
                 </div>
             </div>
         </div>
-    </div>`
+    </div>`;
+
+
     })
-    greedElem.innerHTML = innerGriglia
+    greedElem.innerHTML = innerGriglia;
+    const cardElem = document.querySelectorAll(".ms-card");
+    // console.log(cardElem);
+    cardElem.forEach((curCard) => {
+        const cardImgElem = curCard.querySelector(".card-img-top");
+        console.log(cardImgElem)
+        let cardImgValue = cardImgElem.src;
+        console.log(cardImgValue)
+    //   console.log(curCard);
+      curCard.addEventListener("click", () => {
+        // console.log("click")
+        // console.log(hoverElem)
+        hoverElem.classList.remove("d-none");
+        hoverImgElem.src = cardImgValue;
+      })
+    })
+
+})
+
+closeBtnElem.addEventListener("click", () => {
+    hoverElem.classList.add("d-none")
 })
